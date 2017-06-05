5980329bdd081161b286b421dbf6a3b81db68d14
Phill Conrad <pconrad@cs.ucsb.edu>
2017-06-05 12:38:56 -0700

    update exams

--- _exam/e02/cs56_f16_e02/code/amazoony/Shippable.java
+++
@@ -1,5 +0,0 @@
-/** something that can be shipped */
-public interface Shippable {
-    /** get the shipping weight in pounds */
-    public double getWeight();
-}
--- _exam/e03/cs56_f16_e03/code/amazoony/Product.java
+++
@@ -1,7 +0,0 @@
-/** something that can be sold */
-public abstract class Product {
-    
-    /** get the price (in cents) */
-    public abstract int getPrice();
-    
-}
